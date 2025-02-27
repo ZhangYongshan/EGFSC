8ef7c19b9a6fca2f67d7afa5e106bf2ddd498aee
Yongshan <54622268+ZhangYongshan@users.noreply.github.com>
2025-02-28 00:32:28 +0800

    Add files via upload

---
+++ AXB3_single.m
@@ -0,0 +1,40 @@
+function[acc,label] =  AXB3_single(data3D,label_gt,labelsA,para)
+
+kVal = para.kVal;
+num_PixelA =  para.numPixelA;
+lambda = para.lambda;
+ro = para.ro;
+dim=10; alpha=20;
+
+label_gt = double(label_gt);
+% data3D = double(data3D);
+
+[~,~,P]=size(data3D);
+
+
+data = reshape(data3D, [], P);
+data = mapminmax(data, 0, 1);
+gnd = reshape(label_gt, [], 1);
+nClass = length(unique(gnd)) - 1;
+rand('twister', 3600);
+
+
+% labelsA = cubseg(data3D, num_PixelA);
+[fea] = get_XM(data, labelsA);
+[nSmp, nFea] = size(fea);
+[A, B] = BA(fea, nSmp, nFea, kVal);
+% 执行操作
+W = AXB3(fea', A, B, lambda);
+W = thrC(W, ro);
+[C, L] = postProC(W, nClass, dim, alpha);
+[label] = get_label(data, labelsA, C);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ind = find(gnd);
+gnd_sub = gnd(ind);
+label_sub = label(ind);
+[acc, ~, ~, ~, ~, ~] = evaluate_results_clustering(label_sub, gnd_sub);
+
+
+
+end
